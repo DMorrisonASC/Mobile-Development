5040c769b33d45156c3c20f22698516019ee24a2
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2024-05-03 16:44:14 -0400

    Update listBathrooms.dart

--- HW/final_project/listBathrooms.dart
+++ HW/final_project/listBathrooms.dart
@@ -1,204 +1,260 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+// Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+// Date: 5/3/2024
+// Instructor: Professor Silveyra
+// Description: Code generated from json using https://javiercbk.github.io/json_to_dart/
+// Errors:
 
-import 'model/nearby_response.dart';
+class NearbyPlacesResponse {
+  List<Null>? htmlAttributions;
+  List<Results>? results;
+  String? status;
 
-class ListBathrooms extends StatefulWidget {
-  final ScrollController scrollController;
-  final NearbyPlacesResponse nearbyPlacesResponse;
-
-  const ListBathrooms({required this.scrollController, required this.nearbyPlacesResponse });
-  @override
-  _ListBathroomsState createState() => _ListBathroomsState();
-}
-
-class _ListBathroomsState extends State<ListBathrooms> {
-  final DraggableScrollableController sheetController = DraggableScrollableController();
-
-  @override
-  Widget build(BuildContext context) {
-    print(widget.nearbyPlacesResponse);
-    if (widget.nearbyPlacesResponse.results != null) {
-      debugPrint("Here!");
+  NearbyPlacesResponse({this.results, this.status});
+  NearbyPlacesResponse.fromJson(Map<String, dynamic> json) {
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
     }
-    else{
-      debugPrint("Null!");
-    }
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: Theme.of(context).canvasColor,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
-        ),
-      ),
-      child: widget.nearbyPlacesResponse.results != null ?
-      CustomScrollView(
-        controller: widget.scrollController,
-        slivers: [
-          SliverToBoxAdapter(
-            child: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).hintColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                ),
-                height: 4,
-                width: 40,
-                margin: const EdgeInsets.symmetric(vertical: 10),
-              ),
-            ),
-          ),
-          const SliverAppBar(
-            title: Text('My App'),
-            primary: false,
-            pinned: true,
-            centerTitle: false,
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                var results = widget.nearbyPlacesResponse.results!;
-                return FutureBuilder<Widget>(
-                  future: nearbyPlacesWidget(results[index]),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    } else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      return snapshot.data ?? Container(); // Return the widget or an empty container
-                    }
-                  },
-                );
-              },
-              childCount: widget.nearbyPlacesResponse.results?.length ?? 0, // Return 0 if results is null
-            ),
-          ),
-        ],
-
-      ) : Center(
-        child: CircularProgressIndicator(),
-      ),
-
-    );
+    status = json['status'];
   }
 
-  Future<Widget> nearbyPlacesWidget(Results results) async {
-    // Fetch reviews and wait for completion
-    Map<String, dynamic> reviews = await fetchReviews(results.placeId.toString());
-
-    double rating = 0;
-    int numRating = 0;
-    // Access the reviews in a for loop
-    reviews.forEach((reviewId, reviewData) {
-      print("Review ID: $reviewId");
-      // print("Review data: $reviewData");
-      try {
-          rating += int.parse(reviewData["rating"].toString());
-          numRating += 1;
-      }
-      on Exception catch (e) {
-        print(e);
-      };
-    });
-
-    rating = rating / numRating;
-
-    print(results.placeId!);
-
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-      padding: const EdgeInsets.all(5),
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.deepPurple),
-          borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        children: [
-          Text("Name: " + results.name!),
-          Text("ID: " + results.placeId!),
-          Text("Location: " +
-              results.geometry!.location!.lat.toString() +
-              " , " +
-              results.geometry!.location!.lng.toString()),
-          Text(results.openingHours != null ? "Open" : "Closed"),
-          Text(results.types!.length > 1 ? 'Types ${results.types!}' : 'Types ${results.types!}'),
-          Text(numRating != 0 ? rating.toString() : "No reviews yet"),
-          Icon(Icons.question_mark_outlined),
-        ],
-      ),
-    );
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.results != null) {
+      data['results'] = this.results!.map((v) => v.toJson()).toList();
+    }
+    data['status'] = this.status;
+    return data;
   }
 }
 
-// Future<Map<String, dynamic>> fetchReviews() async {
-//   // Create an empty map to store the reviews
-//   Map<String, dynamic> reviewsMap = {};
-//   CollectionReference collection = FirebaseFirestore.instance.collection('reviews');
-//
-//   try {
-//     // Get the collection reference
-//     QuerySnapshot querySnapshot =
-//     await collection.get();
-//
-//     // Iterate through the documents and store them in the map
-//     for (var doc in querySnapshot.docs) {
-//       // Assuming each document has a unique ID which is used as the key in the map
-//       reviewsMap[doc.id] = doc.data();
-//     }
-//
-//     return reviewsMap;
-//   } catch (e) {
-//     // Handle any errors
-//     print("Error fetching reviews: $e");
-//     return {};
-//   }
-// }
+class Results {
+  Geometry? geometry;
+  String? icon;
+  String? iconBackgroundColor;
+  String? iconMaskBaseUri;
+  String? name;
+  List<Photos>? photos;
+  String? placeId;
+  String? reference;
+  String? scope;
+  List<String>? types;
+  String? vicinity;
+  String? businessStatus;
+  OpeningHours? openingHours;
+  PlusCode? plusCode;
+  dynamic rating;
+  int? userRatingsTotal;
 
-Future<Map<String, dynamic>> fetchReviews(String placeId) async {
-  Map<String, dynamic> reviewsMap = {};
-  CollectionReference collection = FirebaseFirestore.instance.collection('reviews');
+  Results(
+      {this.geometry,
+        this.icon,
+        this.iconBackgroundColor,
+        this.iconMaskBaseUri,
+        this.name,
+        this.photos,
+        this.placeId,
+        this.reference,
+        this.scope,
+        this.types,
+        this.vicinity,
+        this.businessStatus,
+        this.openingHours,
+        this.plusCode,
+        this.rating,
+        this.userRatingsTotal});
 
-  try {
-    // Get the collection reference and query documents where placeId matches
-    QuerySnapshot querySnapshot = await collection.where('placeID', isEqualTo: placeId)
-        .get();
-
-    // Iterate through the documents and store them in the list
-    for (var doc in querySnapshot.docs) {
-      reviewsMap[doc.id] = doc.data();
+  Results.fromJson(Map<String, dynamic> json) {
+    geometry = json['geometry'] != null
+        ? Geometry.fromJson(json['geometry'])
+        : null;
+    icon = json['icon'];
+    iconBackgroundColor = json['icon_background_color'];
+    iconMaskBaseUri = json['icon_mask_base_uri'];
+    name = json['name'];
+    if (json['photos'] != null) {
+      photos = <Photos>[];
+      json['photos'].forEach((v) {
+        photos!.add(Photos.fromJson(v));
+      });
     }
-    print(reviewsMap);
-    return reviewsMap;
-  } catch (e) {
-    // Handle any errors
-    print("Error fetching reviews: $e");
-    return {};
+    placeId = json['place_id'];
+    reference = json['reference'];
+    scope = json['scope'];
+    types = json['types'].cast<String>();
+    vicinity = json['vicinity'];
+    businessStatus = json['business_status'];
+    openingHours = json['opening_hours'] != null
+        ? OpeningHours.fromJson(json['opening_hours'])
+        : null;
+    plusCode = json['plus_code'] != null
+        ? PlusCode.fromJson(json['plus_code'])
+        : null;
+    rating = json['rating'];
+    userRatingsTotal = json['user_ratings_total'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.geometry != null) {
+      data['geometry'] = this.geometry!.toJson();
+    }
+    data['icon'] = this.icon;
+    data['icon_background_color'] = this.iconBackgroundColor;
+    data['icon_mask_base_uri'] = this.iconMaskBaseUri;
+    data['name'] = this.name;
+    if (this.photos != null) {
+      data['photos'] = this.photos!.map((v) => v.toJson()).toList();
+    }
+    data['place_id'] = this.placeId;
+    data['reference'] = this.reference;
+    data['scope'] = this.scope;
+    data['types'] = this.types;
+    data['vicinity'] = this.vicinity;
+    data['business_status'] = this.businessStatus;
+    if (this.openingHours != null) {
+      data['opening_hours'] = this.openingHours!.toJson();
+    }
+    if (this.plusCode != null) {
+      data['plus_code'] = this.plusCode!.toJson();
+    }
+    data['rating'] = this.rating;
+    data['user_ratings_total'] = this.userRatingsTotal;
+    return data;
   }
 }
 
+class Geometry {
+  Location? location;
+  Viewport? viewport;
 
-class FireCollection {
-  static Future<void> createCollectionWithDocument(name) async {
-    await FirebaseFirestore.instance.collection('reviews').add({
-      'name': name,
-      'used': false,
-    });
+  Geometry({this.location, this.viewport});
+
+  Geometry.fromJson(Map<String, dynamic> json) {
+    location = json['location'] != null
+        ? Location.fromJson(json['location'])
+        : null;
+    viewport = json['viewport'] != null
+        ? Viewport.fromJson(json['viewport'])
+        : null;
   }
 
-  void updateCollection(docID, data) {
-    final collection = FirebaseFirestore.instance.collection('pokemon');
-    if (data['used'] == true) {
-      collection.doc(docID).update({'used': false});
-    } else {
-      collection.doc(docID).update({'used': true});
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.location != null) {
+      data['location'] = this.location!.toJson();
     }
+    if (this.viewport != null) {
+      data['viewport'] = this.viewport!.toJson();
+    }
+    return data;
+  }
+}
+
+class Location {
+  double? lat;
+  double? lng;
+
+  Location({this.lat, this.lng});
+
+  Location.fromJson(Map<String, dynamic> json) {
+    lat = json['lat'];
+    lng = json['lng'];
   }
 
-  void deleteDoc(docID) {
-    final collection = FirebaseFirestore.instance.collection('pokemon');
-    collection.doc(docID).delete();
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    return data;
+  }
+}
+
+class Viewport {
+  Location? northeast;
+  Location? southwest;
+
+  Viewport({this.northeast, this.southwest});
+
+  Viewport.fromJson(Map<String, dynamic> json) {
+    northeast = json['northeast'] != null
+        ? Location.fromJson(json['northeast'])
+        : null;
+    southwest = json['southwest'] != null
+        ? Location.fromJson(json['southwest'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.northeast != null) {
+      data['northeast'] = this.northeast!.toJson();
+    }
+    if (this.southwest != null) {
+      data['southwest'] = this.southwest!.toJson();
+    }
+    return data;
+  }
+}
+
+class Photos {
+  int? height;
+  List<String>? htmlAttributions;
+  String? photoReference;
+  int? width;
+
+  Photos({this.height, this.htmlAttributions, this.photoReference, this.width});
+
+  Photos.fromJson(Map<String, dynamic> json) {
+    height = json['height'];
+    htmlAttributions = json['html_attributions'].cast<String>();
+    photoReference = json['photo_reference'];
+    width = json['width'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['height'] = this.height;
+    data['html_attributions'] = this.htmlAttributions;
+    data['photo_reference'] = this.photoReference;
+    data['width'] = this.width;
+    return data;
+  }
+}
+
+class OpeningHours {
+  bool? openNow;
+
+  OpeningHours({this.openNow});
+
+  OpeningHours.fromJson(Map<String, dynamic> json) {
+    openNow = json['open_now'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['open_now'] = this.openNow;
+    return data;
+  }
+}
+
+class PlusCode {
+  String? compoundCode;
+  String? globalCode;
+
+  PlusCode({this.compoundCode, this.globalCode});
+
+  PlusCode.fromJson(Map<String, dynamic> json) {
+    compoundCode = json['compound_code'];
+    globalCode = json['global_code'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['compound_code'] = this.compoundCode;
+    data['global_code'] = this.globalCode;
+    return data;
   }
 }
